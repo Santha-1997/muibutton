fe8ee65028a69b03001dce4a9ce6d1f83b0f9f96
Santha-1997 <santhakumarg@compunet.solutions>
2023-04-27 19:59:09 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css'
+import { MuiButton } from './component/muibutton'
+
+function App() {
+    return (
+      < div className='App'>
+        <MuiButton />
+      </div>
+    )
+}
+
+export default App
